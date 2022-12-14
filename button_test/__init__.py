123e37e8d7eaf4b7efffea9c863fdde12ff01724
Luca Mannella <luca.mannella@polito.it>
2022-12-14 18:38:23 +0000

    updating Python doc

--- button_test/__init__.py
+++ button_test/__init__.py
@@ -1,4 +1,4 @@
-""" This button print some information when pressed. """
+""" This button is used to conduct some tests. It prints some information when pressed. """
 
 # The domain of your component. Should be equal to the name of your component.
 DOMAIN = "button_test"
