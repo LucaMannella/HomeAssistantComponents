6f8ceb7991b943964d47a8cb7f49e5427f9e0df4
Luca Mannella <luca.mannella@polito.it>
2022-12-16 15:53:57 +0000

    adding more information

--- switch_target/__init__.py
+++ switch_target/__init__.py
@@ -1,3 +1,3 @@
-""" A fake switch integration. It is the target of other components' attacks. """
+""" An emulated switch integration. It is the target of other components' attacks. """
 
 DOMAIN = "switch_target"
