b657f764d681d8937c34c1a932d839f8666eb9f2
Luca Mannella <luca.mannella@polito.it>
2022-12-21 13:12:32 +0000

    Introducing improvements in light_altering
    - introducing logging instead of printing
    - removing unused method
    - bug-fixing
    - code more clear and resilient

--- light_altering/light.py
+++ light_altering/light.py
@@ -2,6 +2,7 @@
 from __future__ import annotations
 from typing import Any, Final
 import gc
+import logging
 
 # Import the device class from the component that you want to support
 from homeassistant.core import HomeAssistant
@@ -9,6 +10,11 @@ from homeassistant.components.light import LightEntity
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+NAME_KEY = "name"
+DEFAULT_NAME = "Light Altering"
+
+_LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(
@@ -19,7 +25,12 @@ def setup_platform(
 ) -> None:
     """Set up the Light Altering platform."""
 
-    lights = [LightAltering()]
+    if NAME_KEY in config:
+        name = config[NAME_KEY]
+    else:
+        name = DEFAULT_NAME
+
+    lights = [LightAltering(name)]
     add_entities(lights)
     return True
 
@@ -27,20 +38,19 @@ def setup_platform(
 class LightAltering(LightEntity):
     """Representation of a Light Altering."""
 
-    _target1: Final[str] = "switch.exfiltration_switch"
+    _target1: Final[str] = "switch.switch_exfiltration"
     _target2: Final[str] = "switch.switch_calculated"
 
-    def __init__(self, upload: bool = False) -> None:
+    def __init__(self, name: str = DEFAULT_NAME) -> None:
         """Initialize a LightAltering."""
-        self._name = "Light Altering"
+        self._name = name
         self._brightness = None
         self._state = False
-        self._upload = upload
 
         # This object should physically communicate with the light
         self._light = LightEntity()
 
-        print('Light "' + self._name + '" was created.')
+        _LOGGER.debug("<%s> was created", self._name)
 
     @property
     def name(self) -> str:
@@ -87,39 +97,10 @@ class LightAltering(LightEntity):
         # self._brightness = self._light.brightness
         return
 
-    # async def async_alter_values(self):
-    #     """This method alters values of other integrations."""
-
-    #     # Original State
-    #     t_state = self.hass.states.get(self._target2)
-    #     print(self._target2 + " - actual state: " + t_state.state)
-
-    #     # Modifying State
-    #     if t_state.state == "off":
-    #         await self.hass.states.async_set(entity_id=self._target2, new_state="on")
-    #     else:
-    #         await self.hass.states.async_set(entity_id=self._target2, new_state="off")
-
-    #     # Printing updated state
-    #     t_state = self.hass.states.get(self._target2)
-    #     print(self._target2 + " new state: " + t_state.state)
-
-    #     t_state = self.hass.states.get(self._target1)
-    #     print(self._target1 + " - actual state: " + t_state.state)
-
-    #     if t_state.state == "off":
-    #         self.hass.states.async_set(entity_id=self._target1, new_state="on")
-    #     else:
-    #         self.hass.states.async_set(entity_id=self._target1, new_state="off")
-
-    #     # Retrieving and printing updated state
-    #     t_state = self.hass.states.get(self._target1)
-    #     print(self._target1 + " new state: " + t_state.state)
-
     def alter_values(self):
         """This method alter some values in other integrations."""
-        altering_hello_world = False
-        toggle_calculated_switch = False
+        altering_hello_world = True
+        toggle_calculated_switch = True
         toggle_switch = True
 
         if altering_hello_world:
@@ -127,106 +108,74 @@ class LightAltering(LightEntity):
             hw_domain = "hello_world"
             component_available = self.hass.states.async_available(hw_domain)
             if component_available:
-                target_entity = hw_domain + ".Hello_World"
-                self.hass.states.set(hw_domain + ".Hello_World", "Altered!")
-                self.hass.states.set(hw_domain + ".New_Entity", 42)
-                print(target_entity + " value changed")
+                entity_name = "Hello_World"
+                target_entity = hw_domain + "." + entity_name
+                self.hass.states.set(target_entity, "Altered!")
+                _LOGGER.debug("<%s> altered", target_entity)
+
+                new_entity = hw_domain + ".New_Entity"
+                self.hass.states.set(new_entity, 42)
 
         if toggle_calculated_switch:
-            t_state = self.hass.states.get(self._target2)
-            print(self._target2 + " - actual state: " + t_state.state)
+            # Note: this approach is not working, the updated state is overwritten to the last value few seconds later.
 
-            if t_state.state == "off":
-                self.hass.states.set(entity_id=self._target2, new_state="on")
-            else:
-                self.hass.states.set(entity_id=self._target2, new_state="off")
+            t_state = self.hass.states.get(self._target2)  # Getting current state
+            if t_state:
+                _LOGGER.info("<%s> current state: <%s>", self._target2, t_state.state)
 
-            # Recupero e stampo lo stato aggiornato
-            t_state = self.hass.states.get(self._target2)
-            print(self._target2 + " new state: " + t_state.state)
+                # Toggling the state
+                if t_state.state == "off":
+                    self.hass.states.set(entity_id=self._target2, new_state="on")
+                else:
+                    self.hass.states.set(entity_id=self._target2, new_state="off")
+
+                # Printing new updated state
+                t_state = self.hass.states.get(self._target2)
+                _LOGGER.info("<%s> new state: <%s>", self._target2, t_state.state)
 
         if toggle_switch:
-            # Retrieving and printing the state to alter
+            # In this block I tried several approach
             t_state = self.hass.states.get(self._target1)
-            print(self._target1 + " - actual state: " + t_state.state)
+            if t_state:
+                _LOGGER.info("<%s> current state: <%s>", self._target1, t_state.state)
 
-            # Altering target component
-            if False:
-                # Aggiornamento dello stato forzato.
-                # Non viene recepito dal sistema.
-                if t_state.state == "off":
-                    t_state.state = "on"
-                else:
-                    t_state.state = "off"
-            elif False:
-                # Aggiornamento dello stato tramite metodo setter.
-                # Lo stato viene poi automaticamente sovrascritto (dal componente?)
-                if t_state.state == "off":
-                    self.hass.states.set(entity_id=self._target1, new_state="on")
-                else:
-                    self.hass.states.set(entity_id=self._target1, new_state="off")
-            elif False:
-                # Questo approccio non funziona in quanto accede alla classe statica
-                exf_switch = self.hass.data["integrations"]["exfiltration_switch"]
-                target_switch = exf_switch.get_component().switch
+                # Altering target component
+                if False:
+                    # Aggiornamento dello stato forzato.
+                    # This change seems to be not received by the system.
+                    if t_state.state == "off":
+                        t_state.state = "on"
+                    else:
+                        t_state.state = "off"
+                elif False:
+                    # This approach does not work. It accesses the static class.
+                    exf_switch = self.hass.data["integrations"]["exfiltration_switch"]
+                    target_switch = exf_switch.get_component().switch
 
-                if t_state.state == "on":
-                    target_switch.turn_off()
-                else:
-                    target_switch.turn_on()
-            elif False:
-                # It works! Changing the value accessing the instance through the Garbage Collector
-                for obj in gc.get_objects():
-                    if isinstance(obj, SwitchEntity):
-                        if obj.name == "Exfiltration Switch":
-                            print("I found exf_switch!")
-                            obj.toggle()
-            elif True:
-                self.hass.services.call(
-                    domain="switch",
-                    service="toggle",
-                    service_data={"entity_id": self._target1},
-                )
+                    if t_state.state == "on":
+                        target_switch.turn_off()
+                    else:
+                        target_switch.turn_on()
+                elif False:
+                    # It works! Changing the value accessing the instance through the Garbage Collector
+                    for obj in gc.get_objects():
+                        if isinstance(obj, SwitchEntity):
+                            if obj.name == "Exfiltration Switch":
+                                print("I found exf_switch!")
+                                obj.toggle()
+                elif True:
+                    self.hass.services.call(
+                        domain="switch",
+                        service="toggle",
+                        service_data={"entity_id": self._target1},
+                    )
 
-                # self.hass.services.call(domain="switch.exfiltration_switch", service="toggle")
-                # int_exf_switch = self.hass.data["integrations"]["exfiltration_switch"]
-                # target_component = int_exf_switch.get_component().switch
+                    # self.hass.services.call(domain="switch.exfiltration_switch", service="toggle")
+                    # int_exf_switch = self.hass.data["integrations"]["exfiltration_switch"]
+                    # target_component = int_exf_switch.get_component().switch
 
-            # Recupero e stampo lo stato aggiornato
-            t_state = self.hass.states.get(self._target1)
-            print(self._target1 + " new state: " + t_state.state)
+                # When the state is retrieved is not yet updated, after some seconds the change is triggered.
+                t_state = self.hass.states.get(self._target1)
+                _LOGGER.info("<%s> new state: <%s>", self._target1, t_state.state)
 
         return True
-
-    def read_values(self):
-        """This method reads some values from other integrations."""
-        print_integration = False
-        print_entities = False
-        print_services = False
-
-        if print_integration:
-            # Vengono listate tutte i componenti disponibili
-            print("\nList of available components:")
-            for comp in self.hass.config.components:
-                print(comp)
-            print()
-
-        # Per listare le entità disponibili (istanze dei componenti)
-        if print_entities:
-            n_entities = self.hass.states.async_entity_ids_count()
-            print("\nNumber of available entities: " + str(n_entities))
-
-            print("--- List of filtered entities ---")
-            for ent in self.hass.states.all():
-                # Ho deciso di stampare solo alcune per testing
-                if "switch" in ent.object_id or "light" in ent.object_id:
-                    print(ent)
-            print()
-
-        if print_services:
-            services = self.hass.services.services
-
-            for x, domain in services.items():
-                print("---" + x + "---")
-                for s in domain:
-                    print(s)
