df82db253a44969e53a984cc2c58bc92060f1a6c
Luca Mannella <luca.mannella@polito.it>
2023-04-07 18:46:38 +0200

    Minor updates to be compliant with VS code editor

--- mud_generator/mud_generator.py
+++ mud_generator/mud_generator.py
@@ -24,26 +24,28 @@ _CORE_STORAGE_PATH = "./config/www/MUD/"
 # Filenames
 _DRAFT_FILENAME = "mud_draft.json"
 _FILENAME = "hass_mud_file"
-_MUD_FILENAME = _FILENAME+".json"
-_SIGNATURE_FILENAME = _FILENAME+".p7s"
+_MUD_FILENAME = _FILENAME + ".json"
+_SIGNATURE_FILENAME = _FILENAME + ".p7s"
 _CERTIFICATE_FILENAME = "cert.pem"
 _KEY_FILENAME = "key.pem"
 
-class MUDGenerator():
-    """ This class is able to create and expose a MUD file. """
-    def __init__(self, deployment: str = c.DEPLOY_CORE):
+
+class MUDGenerator:
+    """This class is able to create and expose a MUD file."""
+
+    def __init__(self, deployment: str = c.DEPLOY_CORE) -> None:
         self._deployment = deployment
         if self._deployment == c.DEPLOY_CORE:
             self._STORAGE_PATH = _CORE_STORAGE_PATH
             self._DEFAULT_COMPONENTS_PATH = _CORE_DEFAULT_COMPONENTS_PATH
             self._CUSTOM_COMPONENTS_PATH = _CORE_CUSTOM_COMPONENTS_PATH
-            self._LOCAL_EXTENTION_PATH = _CORE_CUSTOM_COMPONENTS_PATH+"mud_generator/"
+            self._LOCAL_EXTENTION_PATH = _CORE_CUSTOM_COMPONENTS_PATH + "mud_generator/"
         else:
             # HAss OS paths
             self._STORAGE_PATH = "/config/www/MUD/"
             self._DEFAULT_COMPONENTS_PATH = "/homeassistant/components/"
             self._CUSTOM_COMPONENTS_PATH = "/config/custom_components/"
-            self._LOCAL_EXTENTION_PATH = self._CUSTOM_COMPONENTS_PATH+"mud_generator/"
+            self._LOCAL_EXTENTION_PATH = self._CUSTOM_COMPONENTS_PATH + "mud_generator/"
             # Installing openSSL on HAss OS --> maybe this command could not be given in HAss
             installed = os.system("apk add openssl")
             if not installed:
@@ -58,18 +60,19 @@ class MUDGenerator():
         else:
             _LOGGER.debug("Web server folder already exists: <%s>", self._STORAGE_PATH)
 
-    def generate_mud_file(self, integration_list:dict=None, sign:bool=True):
-        """ This function generates a MUD file starting from a template """
+    def generate_mud_file(self, integration_list: dict = None, sign: bool = True):
+        """This function generates a MUD file starting from a template"""
         self._mud_draft = self._load_mud_draft()  # (Re)loading original draft
         inserted_rules = self._add_mud_rules(integration_list)
 
-        mud_file_path = self._LOCAL_EXTENTION_PATH+_MUD_FILENAME
+        mud_file_path = self._LOCAL_EXTENTION_PATH + _MUD_FILENAME
         if not os.path.exists(mud_file_path):
             mud_changed = True
         else:
             with open(mud_file_path, "r", encoding="utf-8") as inputfile:
                 old_mud = json.load(inputfile)
-                if "last-update" in old_mud["ietf-mud:mud"]:  # removing last-update timestamp if exists
+                if "last-update" in old_mud["ietf-mud:mud"]:
+                    # removing last-update timestamp if exists
                     del old_mud["ietf-mud:mud"]["last-update"]
 
                 if self._mud_draft == old_mud:
@@ -79,7 +82,8 @@ class MUDGenerator():
 
         if mud_changed:
             # mud_draft["mud-url"] = "http://iot-device.example.com/dnsname"
-            self._mud_draft["ietf-mud:mud"]["last-update"] = datetime.now().isoformat(timespec="seconds")
+            current_time = datetime.now().isoformat(timespec="seconds")
+            self._mud_draft["ietf-mud:mud"]["last-update"] = current_time
             self._write_mud_file(sign)
         else:
             _LOGGER.debug("MUD file was not changed!")
@@ -88,12 +92,13 @@ class MUDGenerator():
     def _load_mud_draft(self) -> dict:
         draft = None
         # self._cwd = os.getcwd()
-        with open(self._LOCAL_EXTENTION_PATH+_DRAFT_FILENAME, "r", encoding="utf-8") as inputfile:
+        file_name = self._LOCAL_EXTENTION_PATH + _DRAFT_FILENAME
+        with open(file_name, "r", encoding="utf-8") as inputfile:
             draft = json.load(inputfile)
         return draft
 
-    def _add_mud_rules(self, integration_list:dict=None) -> int:
-        """ Adding the ACLs to the MUD object. """
+    def _add_mud_rules(self, integration_list: dict = None) -> int:
+        """Adding the ACLs to the MUD object."""
         inserted_rules = 0
         if integration_list:
             inserted_rules += self._add_rules_from_manifest(integration_list)
@@ -101,30 +106,30 @@ class MUDGenerator():
         _LOGGER.info("Total rules inserted in the MUD file: %d", inserted_rules)
         return inserted_rules
 
-    def _add_rules_from_manifest(self, integration_list:dict):
-        """ This method adds MUD snippets retrieving references from the manifest. """
+    def _add_rules_from_manifest(self, integration_list: dict):
+        """This method adds MUD snippets retrieving references from the manifest."""
         total_inserted_rules = 0
         for integration_name, integration_object in integration_list.items():
             manifest = integration_object.manifest
             if "mud_file" in manifest:
                 mud_snippet = manifest["mud_file"]
                 if mud_snippet.lower().startswith("http"):
-                    _LOGGER.warning("This MUD is remotely stored... Not yet implemented!")
+                    _LOGGER.warning("MUD remotely stored not yet implemented!")
                 else:
                     _LOGGER.debug("MUD snippet of <%s> is stored <%s>", integration_name, mud_snippet)
-                    snippet_path = integration_object.file_path / mud_snippet  # adding to POSIX path
+                    # adding to POSIX path
+                    snippet_path = integration_object.file_path / mud_snippet
                     inserted_rules = self._add_rules_to_draft(snippet_path)
                     _LOGGER.debug("<%s> adds %d rules to the MUD file", integration_name, inserted_rules)
                     total_inserted_rules += inserted_rules
         return total_inserted_rules
 
     def _add_rules_from_folders(self):
-        """ This method takes the MUD snippets crossing integration folders. """
-
+        """This method takes the MUD snippets crossing integration folders."""
         total_inserted_rules = 0
 
         # Iterate over custom components directories
-        _LOGGER.debug("Adding MUD snippets of custom_components...")
+        _LOGGER.debug("Adding MUD snippets of custom_components")
         assert os.path.isdir(self._CUSTOM_COMPONENTS_PATH)
         for cur_path, dirs, files in os.walk(self._CUSTOM_COMPONENTS_PATH):
             if cur_path == self._CUSTOM_COMPONENTS_PATH:
@@ -134,14 +139,14 @@ class MUDGenerator():
 
             if c.MUD_EXTRACT_FILENAME in files:
                 _LOGGER.info("MUD information found in <%s>", cur_path)
-                snippet_path = cur_path+"/"+c.MUD_EXTRACT_FILENAME
+                snippet_path = cur_path + "/" + c.MUD_EXTRACT_FILENAME
                 inserted_rules = self._add_rules_to_draft(snippet_path)
                 _LOGGER.debug("<%s> adds %d rules to the MUD file", cur_path, inserted_rules)
                 total_inserted_rules += inserted_rules
 
         # Iterate over default components directories if available
         if self._deployment == c.DEPLOY_CORE:
-            _LOGGER.debug("Adding MUD snippets of default integrations...")
+            _LOGGER.debug("Adding MUD snippets of default integrations")
             assert os.path.isdir(self._DEFAULT_COMPONENTS_PATH)
             for cur_path, dirs, files in os.walk(self._DEFAULT_COMPONENTS_PATH):
                 if cur_path == self._DEFAULT_COMPONENTS_PATH:
@@ -151,12 +156,12 @@ class MUDGenerator():
 
                 if c.MUD_EXTRACT_FILENAME in files:
                     _LOGGER.info("MUD information found in <%s>", cur_path)
-                    snippet_path = cur_path+"/"+c.MUD_EXTRACT_FILENAME
+                    snippet_path = cur_path + "/" + c.MUD_EXTRACT_FILENAME
                     inserted_rules = self._add_rules_to_draft(snippet_path)
                     _LOGGER.debug("<%s> adds %d rules to the MUD file", cur_path, inserted_rules)
                     total_inserted_rules += inserted_rules
 
-        _LOGGER.info("%d rules were inserted in the generated MUD file", total_inserted_rules)
+        _LOGGER.info("%d rules were inserted in the MUD file", total_inserted_rules)
         return total_inserted_rules
 
     def _add_rules_to_draft(self, snippet_path) -> int:
@@ -182,7 +187,7 @@ class MUDGenerator():
                 new_acls = mud_extract["ietf-access-control-list:acls"]["acl"]
                 acl_count = self._add_acls_if_not_exist(old_acls, new_acls, self._mud_draft["ietf-access-control-list:acls"]["acl"])
 
-                if (from_count+to_count) != acl_count:
+                if (from_count + to_count) != acl_count:
                     _LOGGER.warning("The added snippet is inconsistent!")
                     inserted_rules = -1
                 else:
@@ -195,8 +200,8 @@ class MUDGenerator():
         return inserted_rules
 
     def _add_policies_if_not_exist(self, old_policies, new_policies, target):
-        """ This method adds the <new_policies> inside <target> if they do not exist in <old_policies>.
-            To verify the existence only the field name is checked. """
+        """This method adds the <new_policies> inside <target> if they do not exist in <old_policies>.
+        To verify the existence only the field name is checked."""
         count = 0
         exist = False
         for new_pol in new_policies:
@@ -210,8 +215,8 @@ class MUDGenerator():
         return count
 
     def _add_acls_if_not_exist(self, old_acls, new_acls, target):
-        """ This method adds the <new_acls> inside <target> if they do not exist in <old_acls>.
-            To verify the existence only the field name is checked. """
+        """This method adds the <new_acls> inside <target> if they do not exist in <old_acls>.
+        To verify the existence only the field name is checked."""
         count = 0
         exist = False
         for new_acl in new_acls:
@@ -225,19 +230,19 @@ class MUDGenerator():
         return count
 
     def _write_mud_file(self, sign=True):
-        """ Writing the new MUD file on a JSON file. """
+        """Writing the new MUD file on a JSON file."""
         integration_mud_path = self._LOCAL_EXTENTION_PATH+_MUD_FILENAME
         with open(integration_mud_path, "w", encoding="utf-8") as outfile:
             json.dump(self._mud_draft, outfile, indent=2)
-        _LOGGER.debug("The MUD file has been generated inside the integration folder for debug purposes")
+        _LOGGER.debug("MUD file generated inside integration folder for debug")
 
         if sign:
             _LOGGER.debug("Signing the MUD file")
-            certificate_path = self._LOCAL_EXTENTION_PATH+_CERTIFICATE_FILENAME
-            key_path = self._LOCAL_EXTENTION_PATH+_KEY_FILENAME
+            certificate_path = self._LOCAL_EXTENTION_PATH + _CERTIFICATE_FILENAME
+            key_path = self._LOCAL_EXTENTION_PATH + _KEY_FILENAME
 
             if not os.path.exists(certificate_path) or not os.path.exists(key_path):
-                _LOGGER.debug("Missing X.509 certificate (or private key). Creating right now!")
+                _LOGGER.debug("Missing certificate (or private key). Creating now!")
                 # generating certificate
                 cert_command = 'openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout '+key_path+' -out '+certificate_path+' -subj "/CN=HAss-MUD Integration"'
                 ok = os.system(cert_command)
@@ -253,21 +258,20 @@ class MUDGenerator():
                 ok = os.system(sign_command)
                 if ok == 0:
                     _LOGGER.debug("MUD file signed")
-                    signature_storage_path = self._STORAGE_PATH+_SIGNATURE_FILENAME
+                    signature_storage_path = self._STORAGE_PATH + _SIGNATURE_FILENAME
                     shutil.copyfile(integration_signature_path, signature_storage_path)
                 else:
                     _LOGGER.error("MUD file not signed!")
 
         if os.path.exists(self._STORAGE_PATH):
-            mud_storage_path = self._STORAGE_PATH+_MUD_FILENAME
+            mud_storage_path = self._STORAGE_PATH + _MUD_FILENAME
             shutil.copyfile(integration_mud_path, mud_storage_path)
             _LOGGER.warning("The MUD file is ready to be exposed!")
         else:
             _LOGGER.critical("There is no webserver folder!")
 
-
     def expose_mud_file(self, interface="eth0", mode="DHCP"):
-        """ Exposing the MUD file to the MUD manager. """
+        """Exposing the MUD file to the MUD manager."""
 
         if mode == "DHCP":
             _LOGGER.debug("Exposing the MUD file through DHCP")
@@ -279,9 +283,10 @@ class MUDGenerator():
         else:
             _LOGGER.error("MUD file not exposed, unrecognized mode!")
 
-    def expose_MUD_file_DHCP(self, interface="eth0"):  #wlan0
+    def expose_MUD_file_DHCP(self, interface="eth0"):  # wlan0
+        """This method expose the MUD file using the DHCP protocol."""
         HAss_mac = gma()
-        byte_mac_addr=nu.mac_to_bytes(HAss_mac)
+        byte_mac_addr = nu.mac_to_bytes(HAss_mac)
 
         hostname = socket.gethostname()
         # HAss_IP = socket.gethostbyname(hostname)
@@ -341,12 +346,12 @@ class MUDGenerator():
             dhcp_object
         )
         _LOGGER.debug("Packet to send:\n %s", packet.__str__)
-        x = sendp(packet, iface=interface_name, verbose=verbose, return_packets=True)
-        return x
+        rv = sendp(packet, iface=interface_name, verbose=verbose, return_packets=True)
+        return rv
 
 
     def print_mud_draft(self):
-        """ Printing MUD elements. """
+        """Printing MUD elements."""
 
         _LOGGER.debug("Printing from policies")
         access_list = self._mud_draft["ietf-mud:mud"]["from-device-policy"]["access-lists"]["access-list"]
