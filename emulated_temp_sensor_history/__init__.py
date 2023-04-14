a4cb1e403afcab23720277792871ee0445623b6a
Luca Mannella <luca.mannella@polito.it>
2023-04-14 18:47:32 +0000

    adding a new emulated temperature sensor

---
+++ emulated_temp_sensor_history/__init__.py
@@ -0,0 +1,3 @@
+"""An integration that emulates a temperature sensor. It stores all temperatures in a file."""
+
+DOMAIN = "emulated_temp_sensor_history"
---
+++ emulated_temp_sensor_history/sensor.py
@@ -0,0 +1,116 @@
+"""Platform for sensor integration."""
+from __future__ import annotations
+from typing import Final
+from random import randint
+from datetime import datetime
+import logging
+
+from homeassistant.components.sensor import SensorEntity, DOMAIN as SENSOR_DOMAIN
+from homeassistant.const import UnitOfTemperature
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
+
+from . import DOMAIN
+
+NAME_KEY = "name"
+MIN_TEMP_KEY = "min_temp"
+MAX_TEMP_KEY = "max_temp"
+
+DEFAULT_NAME = "Emulated Temperature Sensor History"
+UNIQUE_ID_PREFIX = "PoliTo.eLite.LM."
+
+""" To work properly this integration needs to have a configured "scan_interval".
+    Every x seconds the update() function is called updating the emulated temperature.
+
+    sensor:
+    - platform: emulated_temp_sensor_history
+      scan_interval: 300
+"""
+_LOGGER = logging.getLogger(__name__)
+
+
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_entities: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
+    """Set up the sensor platform."""
+
+    if NAME_KEY in config:
+        name = config[NAME_KEY]
+    else:
+        name = DEFAULT_NAME
+
+    if (MIN_TEMP_KEY in config) and (MAX_TEMP_KEY in config):
+        add_entities(
+            [
+                EmulatedTempSensorHistory(
+                    name, config[MIN_TEMP_KEY], config[MAX_TEMP_KEY]
+                )
+            ]
+        )
+    elif MIN_TEMP_KEY in config:
+        add_entities([EmulatedTempSensorHistory(name, min_temp=config[MIN_TEMP_KEY])])
+    elif MAX_TEMP_KEY in config:
+        add_entities([EmulatedTempSensorHistory(name, max_temp=config[MAX_TEMP_KEY])])
+    else:
+        add_entities([EmulatedTempSensorHistory(name)])
+
+
+class EmulatedTempSensorHistory(SensorEntity):
+    """Representation of a Sensor. Extends Restore Entity to retrieve last temperature value."""
+
+    def __init__(
+        self, name=DEFAULT_NAME, min_temp: int = 18, max_temp: int = 25
+    ) -> None:
+        """Initialize the sensor."""
+        self._sensor_name = name
+        self._attr_unique_id = UNIQUE_ID_PREFIX + SENSOR_DOMAIN + "." + DOMAIN
+        self._MIN_TMP: Final[int] = min_temp
+        self._MAX_TMP: Final[int] = max_temp
+        self._state = self.compute_random_temp()
+        _LOGGER.info("Initial temperature: %s", str(self._state))
+        self._file_path = (
+            "./custom_components/emulated_temp_sensor_avg/temperatures.txt"
+        )
+        self.append_last_temperature(self._state)
+
+    def compute_random_temp(self) -> float:
+        """This method computes a random initial state."""
+        integer = randint(self._MIN_TMP, self._MAX_TMP - 1)
+        mantissa = randint(0, 9)
+        return float(str(integer) + "." + str(mantissa))
+
+    def append_last_temperature(self, temperature: float) -> None:
+        """This method appends the current state to the consumption file"""
+        with open(self._file_path, "a+", encoding="utf-8") as temperature_file:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            text_to_write = f"{current_time}: {temperature:.2f}\n"
+            temperature_file.write(text_to_write)
+
+    @property
+    def name(self) -> str:
+        """Return the name of the sensor."""
+        return self._sensor_name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def unit_of_measurement(self) -> str:
+        """Return the unit of measurement."""
+        return UnitOfTemperature.CELSIUS
+
+    def update(self) -> None:
+        """Fetch new state data for the sensor.
+
+        This is the only method that should fetch new data for Home Assistant.
+        """
+        new_temp = self.compute_random_temp()
+        _LOGGER.debug("New temp: %.2f", new_temp)
+        self.append_last_temperature(new_temp)
+        self._state = new_temp
